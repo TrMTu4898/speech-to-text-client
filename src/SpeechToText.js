7e31f93732f9a2d35003b8034dc93d4f94472270
TrMTu4898 <112260793+TrMTu4898@users.noreply.github.com>
2023-11-30 19:56:32 +0700

    Update set time send socket

--- src/SpeechToText.js
+++ src/SpeechToText.js
@@ -1,3 +1,6 @@
+import io from 'socket.io-client'
+
+
 const sdk = require("microsoft-cognitiveservices-speech-sdk");
 const SPEECH_KEY  = "3ee8ac5bef9c4ba181422b923dce6fa3"
 const SPEECH_REGION = "southeastasia"
@@ -5,15 +8,24 @@ const speechConfig = sdk.SpeechConfig.fromSubscription(SPEECH_KEY, SPEECH_REGION
 speechConfig.speechRecognitionLanguage = "vi-VN";
 const audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput()
 let recognizer;
+let interimResults = '';
+let resultCallback;
+const socket = io('http://127.0.0.1:5000')
+
 
 const startSpeechRecognition = (onResult) =>{
+    socket.on('connect', ()=>{
+        console.log('Connected to server');
+    })
+    resultCallback = onResult;
     recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig)
     recognizer.recognizing = (_, event)=>{
-        console.log('Interim result:', event.result.text)
+        //console.log('Interim result:', event.result.text)
+        interimResults = event.result.text
     }
     recognizer.recognized =(_, event)=>{
         const finalResult = event.result.text;
-        onResult(finalResult);
+        interimResults += ` ${finalResult}`
     }
     recognizer.startContinuousRecognitionAsync(
         ()=>{},
@@ -21,14 +33,26 @@ const startSpeechRecognition = (onResult) =>{
             console.log('Speech recognition error:', error)
         }
     )
+    setInterval(()=>{
+        if(interimResults){
+            resultCallback(interimResults)
+            socket.emit('speechToTextResult', interimResults)
+            interimResults = ''
+        }
+    }, 0.5 * 60 * 1000 )
 }
 
 const stopSpeechRecognition = () =>{
     if (recognizer) {
         recognizer.stopContinuousRecognitionAsync(
             () => {},
-            () => {}
+            (error) => {
+                console.error('Stop recognition error:', error);
+            }
         );
+    }
+    if (socket){
+        socket.disconnect()
     }
 }
 export { startSpeechRecognition, stopSpeechRecognition };
